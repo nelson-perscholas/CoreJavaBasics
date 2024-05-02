19e66e597586786813b1622b51bd29e95a8e25a8
nelson-perscholas <nelsonchang.peru@gmail.com>
2024-05-01 21:12:30 -0400

    Adding code for GLAB - 303.11.1

---
+++ src/com/perscholas/glab303111/DemoClass.java
@@ -0,0 +1,10 @@
+package com.perscholas.glab303111;
+
+public class DemoClass {
+    // create a generics method
+    public <T> void genericsMethod(T data) {
+        System.out.println("Generics Method:");
+        System.out.println("Data Passed: " + data);
+
+    }
+}
---
+++ src/com/perscholas/glab303111/MyRunner.java
@@ -0,0 +1,32 @@
+package com.perscholas.glab303111;
+
+public class MyRunner {
+    public static void main(String[] args) {
+        System.out.println("********* Testing DemoClass *********");
+
+    // initialize the class with Integer data
+        DemoClass dObj = new DemoClass();
+        dObj.genericsMethod(25); // passing int
+        dObj.genericsMethod("Per Scholas"); // passing String
+        dObj.genericsMethod(2563.5); // passing float
+        dObj.genericsMethod('H'); // passing Char
+
+
+        System.out.println("*****Testing GMultipleDatatype ***********");
+        // initialize generic class
+        // with String and Integer data
+
+        GMultipleDatatype<String, Integer> mobj = new GMultipleDatatype("Per Scholas", 11025);
+
+        System.out.println(mobj.getValueOne());
+        System.out.println(mobj.getValueTwo());
+
+        // initialize generic class
+        // with String and String data
+        GMultipleDatatype<String, String> mobj2 = new GMultipleDatatype("Per Scholas", "Non profit");
+        System.out.println(mobj2.getValueOne());
+        System.out.println(mobj2.getValueTwo());
+
+    }
+
+}
